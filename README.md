6fd5a16083702c9772087039de8f1a4b3bfad0d0
chrisn267 <chris_nightingale@me.com>
2022-01-10 10:57:55 +0000

    testing github link again

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 test after setting up new PAT
 
 this all seems tickety boo!
+
+adding another line for testing...
--- script1.R
+++ script1.R
@@ -3,3 +3,5 @@ assignsomething <- c(1,2,3)
 assignsomething
 
 ### add edit from git
+
+### adding another line here too
